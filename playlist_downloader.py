6b73b74189c93aa082fd9e1df4601c2667df8b68
Godfrey Gudah <gugod254@gmail.com>
2023-01-18 11:05:27 +0300

    changes

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -1,117 +1,44 @@
-# Download each song in a playlist from youtube and save it to a folder
-from lib2to3.pytree import convert
-import os 
-import sys
-from pytube import Playlist
 import youtube_dl
+import os
+import time
 
-# get all the videos in a playlist and store the url's in a list
-def get_playlist_videos(playlist_url):
-    playlist = Playlist(playlist_url)
-    video_urls = playlist.video_urls
-    # print the number of videos in the playlist
-    print("Number of videos in the playlist: " + str(len(video_urls)))
-    return video_urls
-
-def download_songs(song_url, folder_name):
-    if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-            
+def download_playlist(playlist_url, directory):
+    # create directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+    
     ydl_opts = {
-            'format': 'best',
-            'outtmpl': folder_name + '/%(title)s.%(ext)s'
-        }
+        'format': 'bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([song_url])
-        
-            
-# using youtube_dl download all the videos in  the list to a folder
-def download_videos(video_urls, folder_name):
-    if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-    for url in video_urls:
-        ydl_opts = {
-            'format': 'best',
-            'outtmpl': folder_name + '/%(title)s.%(ext)s'
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-                        
-# use youtube_dl to convert the videos to mp3
-def convert_to_mp3(folder_name):
-    # get the list of mp3 files in the folder
-    mp3_files = os.listdir(folder_name)
-    # use youtube_dl to convert the mp3 files to mp3
-    sys.setrecursionlimit(1500)
-    for mp3_file in mp3_files:
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': folder_name + '/%(title)s.%(ext)s',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([mp3_file])
-    
-# main function
-def main():
-    # get the playlist url from the user
-    p = """\t   ########################################################### 
-           ###########################################################
-           ##              YOUTUBE PLAYLIST DOWNLOADER              ##
-           ##                                                       ##
-           ##          Made with Love by Skeeper Loyaltie           ##
-           ##                                                       ##
-           ##                    bugs dont bite                     ##
-           ##                                                       ##
-           ##                      Contribute                       ##
-           ###########################################################
-           ###########################################################
-        
-        Menu: 
-         Welcome to Youtube Playlist Downloader
-        1. Download Mp3: 
-        2. Download Mp4: 
-        3. Exit:
-        
-    
-        """
-    print(p)
-    choice = int(input("\t Enter an option: "))
-    if choice == 1:
-        playlist_url = input("Enter the playlist url: ")
-        
-        # determine if the playlist url is a playlist or a single video
-        if playlist_url.find("list=") != -1:
-            video_urls = get_playlist_videos(playlist_url)
-            folder_name = input("Enter the folder name: ")
-            if not os.path.exists(folder_name):
-                os.makedirs(folder_name)
-                download_videos(video_urls, folder_name)
-            else:
-                print('Directory null.')
-            
-            print("Download complete")
-            
-            # convert the mp4 to mp3
-            # convert_to_mp3(folder_name)       
-        
-        elif playlist_url.find("watch?") != -1:
-            # get the folder name from the user
-            folder_name = input("Enter the folder name: ")
-            # download the video to a folder
-            download_songs(playlist_url, folder_name)
-            print("Download complete")
-            
-            # convert_to_mp3(folder_name)
-            
-            
-        else:
-            print("Invalid playlist url")
-            sys.exit()
-        
-if __name__ == "__main__":
-    main()
+        playlist = ydl.extract_info(playlist_url, download=False)
+        video_list = []
+        for entry in playlist['entries']:
+            video_url = entry['url']
+            video_title = entry['title']
+            video_list.append(f'{video_title}.mp3')
+            if os.path.isfile(f'{video_title}.mp3'):
+                print(f'{video_title}.mp3 already exists, skipping...')
+                continue
+            try:
+                ydl.download([video_url])
+                time.sleep(5)
+            except Exception as e:
+                print(f'Error downloading {video_url}. Retrying...')
+                time.sleep(10)
+                ydl.download([video_url])
+    print('All songs in the playlist have been downloaded.')
+    return video_list
+
+playlist_url = 'https://www.youtube.com/playlist?list=PLWKjhJtqVAbljtmmeS0c-CEl2LdE-eR_F'
+directory = input('Enter directory path: ')
+downloaded_videos = download_playlist(playlist_url, directory)
+print(f'The following videos have been downloaded and saved in {directory}:')
+print(downloaded_videos)
