23eb6c776d288f8368945655d8dc8b10772fa559
skeeperloyaltie <gugod254@gmail.com>
2023-09-28 20:08:20 +0300

    fuck youtube-dl

--- downloader.py
+++ downloader.py
@@ -1,35 +1,34 @@
-from pytube import Playlist
-import os 
+import sys
+import requests
+from bs4 import BeautifulSoup
+import pytube
 
-def download_playlist(playlist_url, directory):
-    # Create a Playlist object
-    playlist = Playlist(playlist_url)
+# Function to extract video links
+def extract_video_links(playlist_url):
+    response = requests.get(playlist_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    links = []
+    
+    for link in soup.find_all('a', {'class': 'yt-simple-endpoint style-scope ytd-playlist-video-renderer'}):
+        video_id = link['href'].split('=')[1]
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+        links.append(video_url)
+    
+    return links
 
-    # Create the directory if it doesn't exist
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    # Change to the specified directory
-    os.chdir(directory)
-
-    video_list = []
-
-    for video in playlist.videos:
+def download_videos(video_links):
+    for link in video_links:
         try:
-            video.streams.filter(only_audio=True, file_extension='mp4').first().download()
-            video_list.append(video.title)
+            yt = pytube.YouTube(link)
+            stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            stream.download(output_path='your_download_path')  # Specify your download path here
+            print("Downloaded:", yt.title)
         except Exception as e:
-            print(f"Error downloading {video.title}: {str(e)}")
+            print("Error downloading:", link, e)
 
-    print('All songs in the playlist have been downloaded.')
-    return video_list
-
-if __name__ == '__main__':
-    playlist_url = input('Enter the URL of the YouTube playlist: ')
-    directory = input('Enter the directory to save the downloaded files: ')
-
-    downloaded_videos = download_playlist(playlist_url, directory)
-
-    print(f'The following videos have been downloaded and saved in {directory}:')
-    for video_title in downloaded_videos:
-        print(video_title)
+if __name__ == "__main__":
+    playlist_url = "https://www.youtube.com/watch?v=T8guj_S9tgs&list=RD6u0DGIh3wLA&index=2&ab_channel=ZoeWeesVEVO"
+    video_links = extract_video_links(playlist_url)
+    print(video_links)
+    download_videos(video_links)
